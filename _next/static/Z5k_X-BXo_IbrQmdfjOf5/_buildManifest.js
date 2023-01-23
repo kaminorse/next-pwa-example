437a6e17b2bea0611f7f741bbec94fcc50e9d0dd
kaminorse <kaminorse@users.noreply.github.com>
2023-01-23 15:58:56 +0000

    deploy: cdb8fd15811afe7fc930154644e1c4d570b9a1bd

--- _next/static/Z5k_X-BXo_IbrQmdfjOf5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/835-3acd1f191e79e84c.js","static/css/d76010dceea002b6.css","static/chunks/pages/index-1bd27e11c310a47f.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/_k6lnH4Uo6BgBkuz9JgM9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/17-64bd9da1c46eadb0.js","static/chunks/pages/index-f4a7224058c168ec.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
