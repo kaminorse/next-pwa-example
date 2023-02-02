4f5a0e0c754fdcd4bf5fad2fd5843e6f36b47ee0
kaminorse <kaminorse@users.noreply.github.com>
2023-02-02 06:58:05 +0000

    deploy: 2b4f9066934b7158c29726f338eaca257de453c2

---
+++ _next/static/T1xhV-TsU-zSYK6t8hmY1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/583-6017c20b84306e36.js","static/chunks/pages/index-880a0820af09f0af.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
