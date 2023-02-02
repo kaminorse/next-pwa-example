0fe9701cb424134225110f296400338292e5d881
Ryo Kamino <contact@kaminorse.com>
2023-02-02 16:06:54 +0900

    change 通知実装の処理を修正

--- src/components/NotificationPermissionButton.tsx
+++ src/components/NotificationPermissionButton.tsx
@@ -2,7 +2,9 @@ import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
 
 export default function NotificationPermissionButton(): JSX.Element {
-  const [permission, setPermission] = useState<NotificationPermission>("default");
+  const [permission, setPermission] = useState<NotificationPermission | null>(
+    null
+  );
 
   useEffect(() => {
     if (typeof Notification !== "undefined") {
@@ -10,20 +12,23 @@ export default function NotificationPermissionButton(): JSX.Element {
       setPermission(currentPermission);
       if (currentPermission === "granted") {
         const notify = () => {
-          const notification = new Notification("next-pwa-exampleの通知テスト", {
-            body: "10秒に一回通知してます。拒否するまで止まりません。",
-          });
+          const notification = new Notification(
+            "next-pwa-exampleの通知テスト",
+            {
+              body: "10秒に一回通知してます。拒否するまで止まりません。",
+            }
+          );
         };
         setTimeout(notify, 10000);
       }
     }
   }, []);
 
-
-
   return (
     <>
-      {permission === "granted" ? (
+      {permission === null ? (
+        <span style={{ color: "red" }}>{"Web通知非対応"}</span>
+      ) : permission === "granted" ? (
         <span style={{ color: "green" }}>{"承認済み"}</span>
       ) : (
         <span>
